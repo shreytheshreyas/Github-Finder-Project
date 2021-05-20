9ce8fb8b0f0c32183d946ae76b943e957a5c7b10
shreytheshreyas <shreyaskumar52@gmail.com>
2021-05-20 13:05:03 +0800

    Created alert

--- app.js
+++ app.js
@@ -13,15 +13,18 @@ searchInput.addEventListener('keyup',(e) => {
             if(userData.profile.message === "Not Found") {
                 //userdata cannot be displayed
                 //dont display anything in DOM
+                uiObj.showAlert("User Does not exist",'alert alert-danger');
             } else {
                 //display user profile data on the DOM
                 //console.log(userData.profile);
+                uiObj.removeAlert();
                 uiObj.showProfile(userData.profile);
 
             }
         });
     } else {
         //The scemario where there is no input
+        uiObj.removeAlert();
         uiObj.displayNothing();
     }
 });
